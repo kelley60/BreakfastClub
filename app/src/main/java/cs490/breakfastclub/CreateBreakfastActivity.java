b6b5ee26257a97f6b0272695f60a9bb6e7733bd8
TrevorEdris <trevor.edris@gmail.com>
2016-10-18 16:18:16 -0400

    Added CreateBreakfastActivity

---
+++ app/src/main/java/cs490/breakfastclub/CreateBreakfastActivity.java
@@ -0,0 +1,18 @@
+package cs490.breakfastclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateBreakfastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_breakfast);
+    }
+
+    void changeBrkfstPic()
+    {
+
+    }
+}
