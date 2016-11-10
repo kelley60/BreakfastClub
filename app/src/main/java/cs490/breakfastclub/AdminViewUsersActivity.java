760515a8280e3848a164071d7e314e4efd55962c
TrevorEdris <trevor.edris@gmail.com>
2016-11-10 16:19:00 -0500

    Made AdminViewUsersActivity functional

--- app/src/main/java/cs490/breakfastclub/AdminViewUsersActivity.java
+++ app/src/main/java/cs490/breakfastclub/AdminViewUsersActivity.java
@@ -3,9 +3,16 @@ package cs490.breakfastclub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -28,12 +35,47 @@ public class AdminViewUsersActivity extends AppCompatActivity {
 
         // TODO: Populate with actual data
         // Populate the list view
+        final ArrayList<User> userList = new ArrayList<>();
+        DatabaseReference memberRef = FirebaseDatabase.getInstance().getReference("Users/");
+        memberRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot d : dataSnapshot.getChildren())
+                {
+                    User u = new User();
+                    u.setName((String) d.child("name").getValue());
+                    u.setProfileImageUrl((String) d.child("profileImageUrl").getValue());
+                    u.setUserId((String) d.getKey());
+                    userList.add(u);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        Log.d("Users", "NUMBER OF USERS: " + userList.size());
+
+
+        /*
         ArrayList<User> userList = new ArrayList<>();
-        for(int i = 0; i < 10; i++)
+        final User currentUser = ((MyApplication) getApplication()).getCurrentUser();
+        if(currentUser != null)
         {
+            userList.add(currentUser);
+            Log.d("Name", currentUser.getName());
+        }
+        else
+        {
+            Log.d("Null", "CURRENT USER IS NULL!!!");
+
+            int i = 0;
             User u = new User("Name " + i, "[url_here]", null);
             userList.add(u);
-        }
+        }*/
 
         UserAdapter uAdapter = new UserAdapter(this, userList);
 
